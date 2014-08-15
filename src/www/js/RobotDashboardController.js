5581254e5bedb7e7969cde7ab4e0dad940777eae
Scott Byrns <contact@scottbyrns.com>
2014-08-15 03:38:30 -0600

    Robot Overview pixel pushing

--- src/www/js/RobotDashboardController.js
+++ src/www/js/RobotDashboardController.js
@@ -3,6 +3,6 @@
 window.robotDashboard.controller("RobotDashboardController", function ($scope, parallaxHelper) {
 	
     $scope.background = parallaxHelper.createAnimator(0.1);
-    $scope.foreground = parallaxHelper.createAnimator(-0.3);
+    $scope.foreground = parallaxHelper.createAnimator(-0.6);
 	
 });
